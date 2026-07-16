7983d85019f7a8fe121bcf9ad500fbe6f5e87906
Max Oltmanns <moltma@pm.me>
2026-07-16 12:17:06 +0200

    fixed bug where next token would be skipped when pushing a constant

--- src/parser.rs
+++ src/parser.rs
@@ -134,7 +134,6 @@ impl Parser {
                                 return;
                             }
                         };
-                        self.tokens.next();
                     }
                     _ => {
                         // if it isn't a name, try to parse a datatype
@@ -174,6 +173,7 @@ impl Parser {
 
             TokenPayload::Store => {
                 let type_t = self.tokens.next().unwrap();
+                println!("{}", type_t);
                 match DType::from_token(&type_t) {
                     Err(s) => {
                         logger.error(&s, type_t.pos().clone());
